5eaa2a78f0a14bc12f4b8014820d949fd4d31325
nellizi <masterliz9095@gmail.com>
2022-07-08 13:48:33 +0900

    테스트 파일 생성

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,35 +1,41 @@
 package com.ll.exam;
 
-import java.sql.SQLOutput;
+
+
 import java.util.Scanner;
 
 public class App {
+
+
 
     public void run() {
         System.out.println("== 명언 SSG ==");
 
         Scanner sc = new Scanner(System.in);
 
+        // 가장 마지막 명언글의 번호
+        int wiseSayingLastId = 0;
+
         outer:
         while (true) {
             System.out.printf("명령) ");
             String cmd = sc.nextLine().trim();
 
-            switch (cmd){
+            switch (cmd) {
                 case "등록":
-                    System.out.println("명언 : ");
+                    System.out.printf("명언 : ");
                     String content = sc.nextLine().trim();
-
-                    System.out.println("작가 : ");
+                    System.out.printf("작가 : ");
                     String author = sc.nextLine().trim();
+                    int id = ++wiseSayingLastId; // 명언 글 번호 증가
 
-                    System.out.println("1번 명언이 등록되었습니다.");
+                    WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                    System.out.println(wiseSaying);
 
-                    break ;
-
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    break;
                 case "종료":
                     break outer;
-
             }
         }
 
