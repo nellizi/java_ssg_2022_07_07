4d838b4bf94f2d480b6a0e11efc30b38def1c8b1
nellizi <masterliz9095@gmail.com>
2022-07-08 14:15:58 +0900

    테스트 함수 완성

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,15 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+
+    @Test
+    public void 테스트_함수(){
+        int rs = 10 + 20 ;
+        assertEquals(30 , rs);
+    }
+}
+
